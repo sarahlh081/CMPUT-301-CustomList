e3e5c11a103ba5a27dff823ff6393635c4654da4
sarah hildt <sarahhildt@gmail.com>
2022-03-10 17:23:34 -0700

    has city pass

--- app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
+++ app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
@@ -56,6 +56,6 @@ public class CustomList extends ArrayAdapter<City> {
         return;
     }
     public boolean hasCity(City city){
-        return false;
+        return cities.contains(city);
     }
 }
