7a8c00072a6c927104e88121491924c2ec03ea3d
sarah hildt <sarahhildt@gmail.com>
2022-03-10 17:18:33 -0700

    fixed again

--- app/src/test/java/CustomListTest.java
+++ app/src/test/java/CustomListTest.java
@@ -4,21 +4,20 @@ import com.example.simpleparadox.listycity.CustomList;
 import org.junit.Before;
 
 import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 
 public class CustomListTest {
-    private CustomList list;
-    @Before
-    public void createList(){
-        list = new CustomList(null,new ArrayList<City>());
-    }
+    private CustomList list  = new CustomList(null,new ArrayList<City>()); //@before kept breaking it
+
 
     @Test
     public void addCityTest(){
         int listSize = list.getCount();
         list.addCity(new City("Edmonton", "AB"));
-        assertEquals(listSize+1,list.getCount());
+        assertEquals(list.getCount(),listSize+1);
     }
 }
